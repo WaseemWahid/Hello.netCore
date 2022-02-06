775fe0f8a576976870b4a8e679dbe09a8e54075c
Waseem Wahid <wahid.waseem@gmail.com>
2022-02-06 11:02:19 -0800

    initial commit for first ever c# program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello.netCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is our first attempt to learning c#!");
+        }
+    }
+}
